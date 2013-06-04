b03125ba8c5031fdb07e9c3d13d426af7e4d96f5
Joe Khoobyar <joe@ankhcraft.com>
2013-06-03 20:55:37 -0400

    added plugin skeleton for Rails 3.2

---
+++ lib/custom_associations.rb
@@ -0,0 +1,6 @@
+require 'active_support/concern'
+require 'active_record/base'
+require 'custom_associations/version'
+
+module CustomAssociations
+end
---
+++ lib/custom_associations/version.rb
@@ -0,0 +1,3 @@
+module CustomAssociations
+  VERSION = "0.1.0"
+end
